1a16accea6af4db7c2dba0befd771b69731778b1
gaelgf <gael.graffion@hotmail.fr>
2016-06-10 18:18:13 +0200

    new question view implemented

--- src/VimoliaBundle/Controller/QuestionsController.php
+++ src/VimoliaBundle/Controller/QuestionsController.php
@@ -11,6 +11,8 @@ namespace VimoliaBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use VimoliaBundle\Entity\Message;
+use VimoliaBundle\Form\MessageType;
 
 class QuestionsController extends Controller
 {
@@ -29,7 +31,24 @@ class QuestionsController extends Controller
      */
     public function displayNewAction()
     {
+
+        $form = $this->createForm(MessageType::class, new Message(),array('method'=>'POST', 'action' => $this->generateUrl('questions_saveMessageForm')));
+
         // replace this example code with whatever you need
-        return $this->render('default/questions/displayNewQuestions.html.twig');
+        return $this->render('default/questions/displayNewQuestions.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/questions/save", name="questions_saveMessageForm")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function saveMessageFormAction(Request $request)
+    {
+
     }
 }
