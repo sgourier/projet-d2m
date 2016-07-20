c70f3a90d806cad2c9b54ca128f657bb1ba5b3a1
gaelgf <gael.graffion@gmail.com>
2016-07-20 02:18:52 +0200

    sous-menu webtv fixed

--- web/js/nav/sous-menu-webtv.js
+++ web/js/nav/sous-menu-webtv.js
@@ -1,13 +1,13 @@
+$(document).ready(function ()
+{
+	$(".sous-menu-webtv").css("top","165px");
 
-
-
-$(".sous-menu-webtv").css("top","165px");
-
-$("#thumb-webtv,.sous-menu-webtv").hover(
-    function(){
-        $(".sous-menu-webtv").css("display","block");
-    },
-    function(){
-        $(".sous-menu-webtv").css("display","none");
-    }
-);
+	$("#thumb-webtv,.sous-menu-webtv").hover(
+	    function(){
+	        $(".sous-menu-webtv").css("display","block");
+	    },
+	    function(){
+	        $(".sous-menu-webtv").css("display","none");
+	    }
+	);
+});
